669d8a4431facfc9783cea30b034ea1eab373e6c
DESKTOP-6KHCC5V\Ron <truong.pn.62cntt@ntu.edu.vn>
2023-05-17 23:01:06 +0700

    push code ngày 17/5/2023

--- TruongVaDuy_WebsiteBanHang/WebsiteBanHang/Controllers/HomeController.cs
+++ TruongVaDuy_WebsiteBanHang/WebsiteBanHang/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using WebsiteBanHang.Context;
@@ -17,6 +20,92 @@ namespace WebsiteBanHang.Controllers
             objHomeModel.ListCategory = objWebsiteBanHangEntities.Categories.ToList();
             objHomeModel.ListProduct = objWebsiteBanHangEntities.Products.ToList();
             return View(objHomeModel);
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        
+        //POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User _user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = objWebsiteBanHangEntities.Users.FirstOrDefault(s => s.Email == _user.Email);
+                if (check == null)
+                {
+                    _user.Password = GetMD5(_user.Password);
+                    objWebsiteBanHangEntities.Configuration.ValidateOnSaveEnabled = false;
+                    objWebsiteBanHangEntities.Users.Add(_user);
+                    objWebsiteBanHangEntities.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exists";
+                    return View();
+                }
+
+
+            }
+            return View();
+
+
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var f_password = GetMD5(password);
+                var data = objWebsiteBanHangEntities.Users.Where(s => s.Email.Equals(email) && s.Password.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["FullName"] = data.FirstOrDefault().FristName + " " + data.FirstOrDefault().LastName;
+                    Session["Email"] = data.FirstOrDefault().Email;
+                    Session["idUser"] = data.FirstOrDefault().Id;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
         }
 
         public ActionResult About()
