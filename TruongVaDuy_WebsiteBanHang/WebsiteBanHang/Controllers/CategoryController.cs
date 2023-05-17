ed316f9f53d866b8e8d8a68bb01f2258f2146d14
DESKTOP-6KHCC5V\Ron <truong.pn.62cntt@ntu.edu.vn>
2023-05-17 19:56:07 +0700

    pussh code

--- TruongVaDuy_WebsiteBanHang/WebsiteBanHang/Controllers/CategoryController.cs
+++ TruongVaDuy_WebsiteBanHang/WebsiteBanHang/Controllers/CategoryController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteBanHang.Context;
 
 namespace WebsiteBanHang.Controllers
 {
     public class CategoryController : Controller
     {
+        WebsiteBanHangEntities objWebsiteBanHangEntities = new WebsiteBanHangEntities();
         // GET: Category
-        public ActionResult Category()
+        public ActionResult Index()
         {
-            return View();
+            var lstCategory = objWebsiteBanHangEntities.Categories.ToList();
+            return View(lstCategory);
+        }
+        public ActionResult ProductCategory(int Id) 
+        {
+            var listProduct= objWebsiteBanHangEntities.Products.Where(n=>n.CategoryId == Id).ToList();
+            return View(listProduct);
         }
     }
 }
--- TruongVaDuy_WebsiteBanHang/WebsiteBanHang/Controllers/ProductController.cs
+++ TruongVaDuy_WebsiteBanHang/WebsiteBanHang/Controllers/ProductController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteBanHang.Context;
 
 namespace WebsiteBanHang.Controllers
 {
     public class ProductController : Controller
     {
+        WebsiteBanHangEntities objWebsiteBanHangEntities = new WebsiteBanHangEntities();
         // GET: Product
-        public ActionResult Detail()
+        public ActionResult Detail(int Id)
         {
-            return View();
+            var objProduct=objWebsiteBanHangEntities.Products.Where(n=>n.Id==Id).FirstOrDefault();
+            return View(objProduct);
         }
     }
 }
