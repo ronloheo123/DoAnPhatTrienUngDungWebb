6679c49e86ece0a95a9e2fa6219b6ec488cef258
DESKTOP-6KHCC5V\Ron <truong.pn.62cntt@ntu.edu.vn>
2023-06-12 21:04:39 +0700

    push code 12/6/2023

---
+++ TruongVaDuy_WebsiteBanHang/WebsiteBanHang/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanHang.Context;
+
+namespace WebsiteBanHang.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        WebsiteBanHangEntities objWebsiteBanHangEntities = new WebsiteBanHangEntities();
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            var lstCategory = objWebsiteBanHangEntities.Categories.ToList();
+            return View(lstCategory);
+        }
+    }
+}
---
+++ TruongVaDuy_WebsiteBanHang/WebsiteBanHang/Areas/Admin/Controllers/OdersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanHang.Context;
+
+namespace WebsiteBanHang.Areas.Admin.Controllers
+{
+    public class OdersController : Controller
+    {
+        WebsiteBanHangEntities objWebsiteBanHangEntities =new WebsiteBanHangEntities();
+        // GET: Admin/Oders
+        public ActionResult Index()
+        {
+            var lstOders = objWebsiteBanHangEntities.Orders.ToList();
+            return View(lstOders);
+        }
+        public ActionResult Details(int id) 
+        {
+            var objOders = objWebsiteBanHangEntities.Orders.Where(n => n.Id == id).FirstOrDefault();
+            
+            return View(objOders);
+        }
+        public ActionResult Edit(int id)
+        {
+            var objOders = objWebsiteBanHangEntities.Orders.Where(n => n.Id == id).FirstOrDefault();
+
+            return View(objOders);
+        }
+        public ActionResult Delete(Order od) 
+        {
+            var objOders = objWebsiteBanHangEntities.Orders.Where(n => n.Id ==od.Id).FirstOrDefault();
+            objWebsiteBanHangEntities.Orders.Remove(objOders);
+            objWebsiteBanHangEntities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
